0cdbfe6fc85e97e26559d89afb552b13f2529fbc
mazrean <shunsuke.w04@gmail.com>
2024-09-28 03:34:29 +0900

    wrap getConfiguration

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { registerCommand, config } from "./vscode-wrapper";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -14,17 +15,11 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    "typed-hello-world.helloWorld",
-    () => {
-      const message =
-        vscode.workspace
-          .getConfiguration()
-          .get<string>("typed-hello-world.message") || "Hello World!";
+  const disposable = registerCommand("typed-hello-world.helloWorld", () => {
+    const message = config("typed-hello-world.message");
 
-      vscode.window.showInformationMessage(message);
-    }
-  );
+    vscode.window.showInformationMessage(message);
+  });
 
   context.subscriptions.push(disposable);
 }
--- src/vscode-wrapper.ts
+++ src/vscode-wrapper.ts
@@ -8,3 +8,31 @@ export const registerCommand = (
   command: Command,
   callback: (...args: any[]) => any
 ) => vscode.commands.registerCommand(command, callback);
+
+const conf = packageJson.contributes.configuration.properties;
+type Config = typeof conf;
+type TypeMap = {
+  boolean: boolean;
+  string: string;
+  number: number;
+};
+type ConvertType<T extends keyof Config> = TypeMap[Config[T]["type"]];
+
+export const config = <T extends keyof Config>(key: T) => {
+  let defaultValue: ConvertType<T>;
+  switch (conf[key].type as "string" | "boolean" | "number") {
+    case "string":
+      defaultValue = conf[key].default as unknown as ConvertType<T>;
+      break;
+    case "boolean":
+      defaultValue = Boolean(conf[key].default) as unknown as ConvertType<T>;
+      break;
+    case "number":
+      defaultValue = Number(conf[key].default) as unknown as ConvertType<T>;
+      break;
+  }
+
+  return (
+    vscode.workspace.getConfiguration().get<ConvertType<T>>(key) ?? defaultValue
+  );
+};
